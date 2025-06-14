1108a3e762311630873ec41b84de00d4b2668aae
nertyqwerty <subalex1@MacBookAleksandr.local>
2025-06-14 21:46:16 +0300

    Добавил вторую функцию

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,22 @@
 const stringLength = (myString, maxLength) => myString.length <= maxLength;
 
 console.log(stringLength('ololoolo', 5));
+
+
+const polindromCheck = (myString) => {
+  const normalizeString = myString.toLowerCase().replaceAll(' ', '');
+  for (let i = 0; i < normalizeString.length; i++) {
+    if (normalizeString[i] !== normalizeString[normalizeString.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(polindromCheck('Тор ттр оТ'));
+
+
+// const ddddd = (myString) => {
+//  let eeee =  myString.length;
+//  return eeee;
+// }
+// console.log(ddddd('шалаш'));
